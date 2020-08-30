39624e09263bb9d62ca141668c110bba00e98cb2
Adarsh Dwivedi <addush456@gmail.com>
2020-08-30 23:23:59 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Adarsh Dwivedi",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "A budding Computer Scientist with a variety of domain knowledge. Adept in building visual insights and statistical models using R, Python and Tableau. Passionate about data analysis and a keen learner in the field of Data Science.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/adarsh-dwivedi-425324148/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/adarsh1223",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at Lovely Professional University, Phagwara and pursuing my B.Tech (Computer Science and Engineering) from here. A budding Computer Scientist with a variety of domain knowledge. Adept in building visual insights and statistical models using R, Python and Tableau. Passionate about data analysis and a keen learner in the field of Data Science.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Lovely Professional University",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Bethany Convent School",
+        "specialization":"Non-Medical",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2017",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"DataCamp",
+        "specialization":"Data Scientist With Python",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Coursera",
+        "specialization":"Data Structures and Algorithms",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
